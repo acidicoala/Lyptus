5e31a5a71a07e061543465e78632d2a3a4ba81c9
acidicoala <67734819+acidicoala@users.noreply.github.com>
2022-02-03 20:52:32 +0200

    Added pattern validation

--- src/config/config.cpp
+++ src/config/config.cpp
@@ -3,8 +3,51 @@
 
 #include <fstream>
 #include <memory>
+#include <set>
+#include <algorithm>
 
 namespace config {
+
+    void fatal_crash(const String& message) {
+        util::error_box(__func__, message);
+        exit(::GetLastError()); // NOLINT(cppcoreguidelines-narrowing-conversions)
+    }
+
+    bool contains_only(const String& string, const std::set<char>& whitelist) {
+        return std::ranges::all_of(string.begin(), string.end(),
+            [&](char symbol) {
+                return whitelist.contains(symbol);
+            }
+        );
+    }
+
+    void validate_config(const Config& config) {
+        std::set<char> valid_replacement_chars{
+            '0', '1', '2', '3', '4', '5', '6', '7',
+            '8', '9', 'A', 'B', 'C', 'D', 'E', 'F',
+            '?', ' ', '\t'
+        };
+
+        std::set<char> valid_pattern_chars(valid_replacement_chars);
+        valid_pattern_chars.insert({ '?', ' ', '\t' });
+
+        for (const auto& patch: config.patches) {
+            if (not contains_only(patch.pattern, valid_pattern_chars)) {
+                fatal_crash(fmt::format(
+                    "Patch '{}' contains invalid pattern: '{}'",
+                    patch.name, patch.pattern
+                ));
+            }
+
+            if (not contains_only(patch.pattern, valid_replacement_chars)) {
+                fatal_crash(fmt::format(
+                    "Patch '{}' contains invalid replacement: '{}'",
+                    patch.name, patch.replacement
+                ));
+            }
+        }
+    }
+
 
     Config read(Path path) { // NOLINT(performance-unnecessary-value-param)
         if (not std::filesystem::exists(path)) {
@@ -16,12 +59,14 @@ namespace config {
             nlohmann::json json;
             ifs >> json;
 
-            return json.get<Config>();
+            Config config = json.get<Config>();
 
-            // TODO: Validate pattern & replacement strings;
+            validate_config(config);
+
+            return config;
         } catch (const std::exception& ex) {
-            util::error_box(__func__, fmt::format("Failed to parse config file: {}", ex.what()));
-            exit(::GetLastError()); // NOLINT(cppcoreguidelines-narrowing-conversions)
+            fatal_crash(fmt::format("Failed to parse config file: {}", ex.what()));
+            return {};
         }
     }
 
